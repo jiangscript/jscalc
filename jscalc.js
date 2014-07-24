c745ecbc4bf8c96b126af4b0243feb79c88fbf81
James Davidson <james@cultureamp.com>
2014-07-24 18:13:09 +1000

    Adjusts whitespace

--- jscalc.js
+++ jscalc.js
@@ -1,21 +1,21 @@
 var state = '';
 
 function update() {
-	try {
-		$('#result').val(eval(state));
-	}
-	catch (exception) {
-		$('#result').val(state);
-	}
+  try {
+    $('#result').val(eval(state));
+  }
+  catch (exception) {
+    $('#result').val(state);
+  }
 }
 
 $('.number, .operator').click(function() {
   state = String(state) + $(this).text();
-	update()
+  update()
 });
 
 $('#equals').click(function() { 
-	update();
+  update();
 });
 
 $('#clear').click(function(){
